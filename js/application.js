26fbc709fe9dd8aab2b954a9edfe35520b9814ad
betsyrhodes <betzrhodes@gmail.com>
2014-08-25 13:51:01 -0700

    sound portfolio bar formatting roughed in

--- js/application.js
+++ js/application.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('td').hover(show, hide);
+  $('#sound-bar img').hover(showPress, hidePress);
 });
 
 var show = function() {
@@ -10,4 +11,20 @@ var show = function() {
 var hide = function() {
   $(this.firstElementChild).addClass("hidden");
   $(this.firstElementChild).removeClass("dim-bkgnd");
+};
+
+var showPress = function() {
+  var id = this.dataset.id;
+  var project = $('.project-info[data-id='+ id +']');
+  $(this).addClass("dim-bkgnd");
+  project.removeClass("hidden");
+  // project.addClass("dim-bkgnd");
+};
+
+var hidePress = function() {
+  var id = this.dataset.id;
+  var project = $('.project-info[data-id='+ id +']');
+  // project.removeClass("dim-bkgnd");
+  project.addClass("hidden");
+  $(this).removeClass("dim-bkgnd");
 };
