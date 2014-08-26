8e43755acd92bc30269018bfe83e61eb7bee58a9
betsyrhodes <betzrhodes@gmail.com>
2014-08-26 12:03:48 -0700

    web portfolio description reveal incorporated

--- js/application.js
+++ js/application.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
-  $('td').hover(show, hide);
-  $('#sound-bar img').hover(showPress, hidePress);
+  $('#sound-bar img').hover(showSndPress, hideSndPress);
+  $('#web-portfolio .project-info').hover(showWebDesc, hideWebDesc);
 });
 
-var show = function() {
-  $(this.firstElementChild).removeClass("hidden");
-  $(this.firstElementChild).addClass("dim-bkgnd");
+var showWebDesc = function() {
+  $(this.lastElementChild).removeClass("hidden");
 };
 
-var hide = function() {
-  $(this.firstElementChild).addClass("hidden");
-  $(this.firstElementChild).removeClass("dim-bkgnd");
+var hideWebDesc = function() {
+  $(this.lastElementChild).addClass("hidden");
 };
 
-var showPress = function() {
+var showSndPress = function() {
   var id = this.dataset.id;
   var project = $('.project-info[data-id='+ id +']');
+
   $(this).addClass("dim-bkgnd");
   $('.logo').addClass("hidden");
   project.removeClass("hidden");
   // project.addClass("dim-bkgnd");
 };
 
-var hidePress = function() {
+var hideSndPress = function() {
   var id = this.dataset.id;
   var project = $('.project-info[data-id='+ id +']');
   // project.removeClass("dim-bkgnd");
